f11622e4b2b74b61a2e6c8c3d342b32e7fc2dd20
luis-moguea <luis.mogueadp@gmail.com>
2023-03-02 19:18:52 -0500

    general updates

--- test.js
+++ test.js
@@ -2,13 +2,26 @@
 //of value in an expected result
 var fruitsVegetables = function (fruits, vegetables, quantity) {
     var result;
-    if (typeof (fruits) === "number" && typeof (vegetables) === "number" && typeof (quantity) === "number") {
+    if (typeof (fruits) === "number" && typeof (vegetables) === "number") {
         result = fruits + vegetables + quantity;
     }
     else {
-        result = fruits.toString() + vegetables.toString() + quantity.toString();
+        result = fruits.toString() + vegetables.toString() + quantity;
     }
     return result;
 };
-//const whatIlike = fruitsVegetables("bananas","tomatoes", 8)
-console.log(fruitsVegetables("bananas ", "tomatoes ", 8));
+var whatIlike = fruitsVegetables("Banana", "Tomatoes", 8);
+console.log(whatIlike);
+var fruits = ["Bananas ", "Mangoes ", "Pears "];
+var vegetables = ["Tomatoes", "Potatoes", "Broccoli"];
+var testing = function (fruits, vegetables, quantity) {
+    var result;
+    if (typeof (fruits) === "number" && typeof (vegetables) === "number") {
+        result = fruits + vegetables + quantity;
+    }
+    else {
+        result = fruits.toString() + vegetables.toString() + quantity;
+    }
+    return result;
+};
+console.log(testing(fruits, vegetables, 8));
--- test.ts
+++ test.ts
@@ -3,14 +3,30 @@
 
 const fruitsVegetables = (fruits: number | string, vegetables:number | string, quantity:number) => {
   let result;
-  if (typeof(fruits) === "number" && typeof(vegetables) === "number" && typeof(quantity) === "number") {
+  if (typeof(fruits) === "number" && typeof(vegetables) === "number") {
     result = fruits + vegetables + quantity;
   } else {
-    result = fruits.toString() + vegetables.toString() + quantity.toString()
+    result = fruits.toString() + vegetables.toString() + quantity
   }
   return result;
 };
 
-//const whatIlike = fruitsVegetables("bananas","tomatoes", 8)
+const whatIlike = fruitsVegetables("Banana", "Tomatoes", 8)
 
-console.log(fruitsVegetables("bananas ","tomatoes ", 8))
+console.log(whatIlike)
+
+
+const fruits: string[] = ["Bananas ", "Mangoes ", "Pears "]
+const vegetables: string[] = ["Tomatoes", "Potatoes", "Broccoli"]
+
+const testing = (fruits: string[], vegetables: string[], quantity:number) => {
+    let result;
+    if (typeof(fruits) === "number" && typeof(vegetables) === "number") {
+        result = fruits + vegetables + quantity;
+      } else {
+        result = fruits.toString() + vegetables.toString() + quantity
+      }
+      return result;
+  };
+
+  console.log(testing(fruits, vegetables, 8))
